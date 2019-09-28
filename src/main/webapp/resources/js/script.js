2ccb55a1e9a8fddb643c19f9bcabb3e1d9f148d2
FernandoAndradeSilva <fernando.andrade.inf@gmail.com>
2019-09-28 19:20:01 -0300

    finalizando-nova-pagina-cadastro-item-epesquisa

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -11,3 +11,12 @@
 jQuery("#formCadastroItem\\:bt").click(function () {
     alert('Clicadop');
 });
+
+
+
+    $("#formCat\\:dtList\\:pesquisar").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#formCat\\:dtList div > ul > li").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
